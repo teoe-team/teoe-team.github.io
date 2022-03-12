d89d5b1ba35c7851e53468764a471df8448fbefb
Aiden <95098897+AidenKRP99@users.noreply.github.com>
2022-03-12 17:01:15 -0600

    amogus
    sussy baka is susoiki9juhyhju8jki9

---
+++ signin.js
@@ -0,0 +1,7 @@
+var username = document.getElementById("username")
+var password = document.getElementById("password")
+function signIn(username,password) {
+	alert('This is a test, OK?')
+	console.log(username.value)
+	console.log(password.value)
+}
